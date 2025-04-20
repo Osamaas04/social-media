80d6ba7c008223541d2311e6a0ed7d6d9959c2ce
Osamaas04 <alasmarosama7@gmail.com>
2025-04-20 18:11:10 +0300

    refine 53

--- utils/getUserIdFromToken.js
+++ utils/getUserIdFromToken.js
@@ -7,11 +7,16 @@ export const getUserIdFromToken = (request) => {
     .find(c => c.trim().startsWith('token='))
     ?.split('=')[1];
 
+    console.log(token)
+
   if (!token) {
     throw new Error('Unauthorized: No token');
   }
 
   const decoded = jwt.decode(token);
+
+  console.log(decoded)
+  console.log(decoded.sub)
 
   if (!decoded || !decoded.sub) {
     throw new Error('Unauthorized: Invalid token');
